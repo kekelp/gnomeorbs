5e1a87912124a6deccb75c4c52799afa8b63c754
kekelp <kekelp@kekelp>
2024-09-14 00:21:33 +0200

    15

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use errors::*;
 
 mod icon;
 
-mod helper;
-use helper::*;
+mod extra;
+use extra::*;
 
 use std::env;
 use std::error;
@@ -60,9 +60,9 @@ const DESK_EXT: &str = "desktop";
 const ICON_EXT: &str = "png";
 
 fn select_exe_file(args: &Args) -> Result<Utf8PathBuf> {
-    let bin_file_path = Utf8Path::new(&args.bin_file).canonicalize_utf8()?;
+    let exe_file = Utf8Path::new(&args.bin_file).canonicalize_utf8()?;
 
-    let metadata = fs::metadata(bin_file_path.clone())?;
+    let metadata = fs::metadata(exe_file.clone())?;
 
     if metadata.is_file() == false {
         return Err(Box::new(NotAFileError));
@@ -75,16 +75,16 @@ fn select_exe_file(args: &Args) -> Result<Utf8PathBuf> {
 
     println!(
         "selected executable file:\n    {}",
-        bin_file_path
+        exe_file
     );
 
-    return Ok(bin_file_path);
+    return Ok(exe_file);
 }
 
 fn process() -> Result<()> {
     let args = Args::parse();
 
-    let bin_file_path = select_exe_file(&args)?; 
+    let exe_file = select_exe_file(&args)?; 
 
     let home_dir = env::var("HOME")?;
     let home_dir = Utf8Path::new(&home_dir);
@@ -92,22 +92,22 @@ fn process() -> Result<()> {
     let local_icons_path = home_dir.join(Utf8Path::new(REL_LOCAL_ICONS_PATH));
 
 
-    let bin_file_name = bin_file_path.file_name().ok_or(NotAFileError)?;
-    let title_case_bin_file_name = bin_file_name.to_case(Case::Title);
-    let desk_file_stem: Utf8PathBuf = bin_file_name.into();
+    let exe_filename = exe_file.file_name().ok_or(NotAFileError)?;
+    let title_case_exe_filename = exe_filename.to_case(Case::Title);
+    let desktop_file_stem: Utf8PathBuf = exe_filename.into();
 
-    let desk_file_path = local_apps_path
-        .join(&desk_file_stem)
+    let desktop_file_path = local_apps_path
+        .join(&desktop_file_stem)
         .with_extension(DESK_EXT);
 
-    println!("target .desktop file:\n    {}", desk_file_path);
+    println!("target .desktop file:\n    {}", desktop_file_path);
 
-    let desk_file_opt = OpenOptions::new()
+    let desktop_file_opt = OpenOptions::new()
         .write(true)
         .create_conditional(args.overwrite)
-        .open(desk_file_path.clone());
+        .open(desktop_file_path.clone());
 
-    let mut desk_file = match desk_file_opt {
+    let mut desktop_file = match desktop_file_opt {
         Ok(df) => df,
         Err(err) => {
             if err.kind() == ErrorKind::AlreadyExists {
@@ -118,23 +118,23 @@ fn process() -> Result<()> {
     };
 
     let icon_path = local_icons_path
-        .join(&desk_file_stem)
+        .join(&desktop_file_stem)
         .with_extension(ICON_EXT);
     println!("generating icon:\n    {}", icon_path);
     create_dir_all(local_icons_path)?;
-    icon::draw_and_save_icon(bin_file_name, &icon_path);
+    icon::draw_and_save_icon(exe_filename, &icon_path);
 
-    let mut buffer: String = String::with_capacity(800);
+    let mut buffer = String::with_capacity(800);
 
     buffer.push_str(&template::HEADER);
 
-    buffer.pushln2("Name=", &title_case_bin_file_name);
-    buffer.pushln2("Exec=", bin_file_path.as_str());
+    buffer.pushln2("Name=", &title_case_exe_filename);
+    buffer.pushln2("Exec=", exe_file.as_str());
     buffer.pushln2("Icon=", &icon_path.as_str());
 
     // todo: this is flipped lol
     if args.skip_misspell {
-        let keywords = misspellings(bin_file_name).join(",");
+        let keywords = misspellings(exe_filename).join(",");
         buffer.pushln2("Keywords=", &keywords);
     } else {
         buffer.pushln("# Keywords=");
@@ -148,12 +148,12 @@ fn process() -> Result<()> {
 
     buffer.push_str(&template::EMPTY_LINES);
 
-    desk_file.write_all(buffer.as_bytes())?;
-    desk_file.flush()?;
+    desktop_file.write_all(buffer.as_bytes())?;
+    desktop_file.flush()?;
 
     if args.edit {
         println!("Opening target file with default editor...");
-        edit::edit_file(desk_file_path)?;
+        edit::edit_file(desktop_file_path)?;
     }
 
     return Ok(());
